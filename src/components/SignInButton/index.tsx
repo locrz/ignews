364ed60c1c3f495d8924352ee857bfa278e1e338
Lucas Oliveira <lucas.oliveira.timburi@gmail.com>
2021-06-28 08:45:28 -0300

    feat: add authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [session] = useSession();
 
-  function toggleButton() {
-    setIsUserLoggedIn((prev) => !prev);
+  function onButtonClick() {
+    signIn("github");
   }
 
-  if (isUserLoggedIn) {
+  if (session) {
     return (
       <button
         className={styles.signInButton}
         type="button"
-        onClick={toggleButton}
+        onClick={() => signOut()}
       >
         <FaGithub color="#84d361" />
-        Lucas Oliveira
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
     );
@@ -29,7 +30,7 @@ export function SignInButton() {
     <button
       className={styles.signInButton}
       type="button"
-      onClick={toggleButton}
+      onClick={onButtonClick}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
