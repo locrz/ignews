fcdb08782402cc6776c6792f9a144c993fa484c9
Lucas Oliveira <lucas.oliveira.timburi@gmail.com>
2021-07-22 08:34:21 -0300

    feat: creating post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 interface Post {
   slug: string;
@@ -27,11 +28,13 @@ export default function Posts({ posts }: PostProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a href="" key={post.slug}>
-              <time>{post.updated_at}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updated_at}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
